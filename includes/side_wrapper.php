582b46d40490d306f42325b6dfc711bdf168c8e5
jeffreytraballo <traballo.j.bsinfotech@gmail.com>
2025-01-22 19:45:39 +0800

    Add files via upload

---
+++ includes/side_wrapper.php
@@ -0,0 +1,18 @@
+<div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="index1.php" class="simple-text">
+                    HOME
+                </a>
+            </div>
+
+          
+				<!--<li>
+                    <a href="#">
+                        <i class="pe-7s-news-paper"></i>
+                        <p>Gallery</p>
+                    </a>
+                </li>-->
+                
+            </ul>
+    	</div>
+    </div>
